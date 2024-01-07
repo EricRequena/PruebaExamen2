bece6e6ac5049ea76bd457e2f182ccbb5c5ed484
EricRequena <eric.requena.7e7@itb.cat>
2024-01-07 18:30:13 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PracticaExamen
+{
+    public class Ejercicio1
+    {
+        public static void Main()
+        {
+            const string MsgIntMinuts = "Introduce una cantidad en minutos";
+            double minuts, c = 60;
+
+            Console.WriteLine(MsgIntMinuts);
+            minuts = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"La conversion es {conversion(minuts * c)} segons");
+        }
+        public static double conversion(double seconds)
+        {
+            return seconds;
+        }
+    }
+}
